613c1bd77bd62b54cd83a244b93ccc533660cc0f
Arkaitz Amostegui <ikdge@plaiaundi.net>
2025-11-23 08:37:25 +0100

    migrar BDD

--- src/database/migrations/2025_11_17_000001_create_productos_table.php
+++ src/database/migrations/2025_11_17_000001_create_productos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nombre');
             $table->decimal('precio', 8, 2)->nullable(); 
             $table->integer('stock')->default(0);
-             
+            
             // Relación con categorías
             $table->unsignedBigInteger('idCategoria');
             $table->foreign('idCategoria')->references('id')->on('categorias')->onDelete('cascade');
